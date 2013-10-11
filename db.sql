c65ec9964486adbb5f6cc133c42b9cde0c358b52
Artem <artbonvic@gmail.com>
2013-10-12 02:16:33 +0300

    Hello

---
+++ db.sql
@@ -0,0 +1,187 @@
+-- MySQL dump 10.13  Distrib 5.5.32, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: dictator
+-- ------------------------------------------------------
+-- Server version	5.5.32-0ubuntu7
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `author`
+--
+
+DROP TABLE IF EXISTS `author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `author` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `author`
+--
+
+LOCK TABLES `author` WRITE;
+/*!40000 ALTER TABLE `author` DISABLE KEYS */;
+INSERT INTO `author` VALUES (1,'Автор 1'),(2,'Автор 2'),(3,'Автор 3');
+/*!40000 ALTER TABLE `author` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `author_book`
+--
+
+DROP TABLE IF EXISTS `author_book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `author_book` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `authorId` int(11) NOT NULL,
+  `bookId` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `authorId` (`authorId`),
+  KEY `bookId` (`bookId`),
+  CONSTRAINT `author_book_ibfk_1` FOREIGN KEY (`authorId`) REFERENCES `author` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `author_book_ibfk_2` FOREIGN KEY (`bookId`) REFERENCES `book` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `author_book`
+--
+
+LOCK TABLES `author_book` WRITE;
+/*!40000 ALTER TABLE `author_book` DISABLE KEYS */;
+INSERT INTO `author_book` VALUES (4,1,2),(6,3,4),(12,1,1),(13,2,1),(14,3,1),(52,2,3),(61,1,20),(62,2,20),(63,2,5),(64,1,22),(65,3,22);
+/*!40000 ALTER TABLE `author_book` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `book` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `categoryId` int(11) DEFAULT NULL,
+  `pic` varchar(255) DEFAULT NULL,
+  `desc` longtext,
+  `date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `categoryId` (`categoryId`),
+  CONSTRAINT `book_ibfk_1` FOREIGN KEY (`categoryId`) REFERENCES `category` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book`
+--
+
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+INSERT INTO `book` VALUES (1,'Книга 1',1,NULL,NULL,NULL),(2,'Книга 2',1,NULL,NULL,NULL),(3,'Книга 3',1,NULL,NULL,NULL),(4,'Книга 4',1,NULL,NULL,NULL),(5,'Книга 5',1,NULL,'Описание',NULL),(20,'sadasd',2,NULL,'Национальная футбольная сборная Украины сегодня сыграет с командой Польши отборочный матч к Чемпионату мира-2014. Игра состоится в Харькове на стадионе \"Металлист\" и начнется в 21:00, прямая трансляция - на телеканале Интер.\r\n\r\nДо завершения квалификации остается всего два матча, но ситуация в нашей турнирной группе остается очень запутанной. На первое место и прямую путевку в Бразилию претендуют четыре команды и как раз сегодня претенденты играют друг с другом.','2013-12-10 10:10:10'),(22,'Тест',1,'525884d671b23.jpg','Описание тест','2010-12-12 10:10:10');
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'Категория 1'),(2,'Категория 2'),(3,'Категория 3');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `publisher`
+--
+
+DROP TABLE IF EXISTS `publisher`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `publisher` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `publisher`
+--
+
+LOCK TABLES `publisher` WRITE;
+/*!40000 ALTER TABLE `publisher` DISABLE KEYS */;
+INSERT INTO `publisher` VALUES (1,'Издательство 1'),(2,'Издательство 2'),(3,'Издательство 3'),(4,'Издательство 4'),(5,'Издательство 5'),(6,'Издательство 6'),(7,'Издательство 7'),(8,'Издательство 8'),(9,'Издательство 9'),(10,'Издательство 10'),(11,'Издательство 11'),(12,'Издательство 12'),(13,'Издательство 13'),(14,'Издательство 14'),(15,'Издательство 15');
+/*!40000 ALTER TABLE `publisher` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `publisher_book`
+--
+
+DROP TABLE IF EXISTS `publisher_book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `publisher_book` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bookId` int(11) NOT NULL,
+  `publisherId` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `bookId` (`bookId`),
+  KEY `publisherId` (`publisherId`),
+  CONSTRAINT `publisher_book_ibfk_1` FOREIGN KEY (`bookId`) REFERENCES `book` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `publisher_book_ibfk_2` FOREIGN KEY (`publisherId`) REFERENCES `publisher` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=103 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `publisher_book`
+--
+
+LOCK TABLES `publisher_book` WRITE;
+/*!40000 ALTER TABLE `publisher_book` DISABLE KEYS */;
+INSERT INTO `publisher_book` VALUES (1,1,1),(2,1,3),(50,3,1),(51,3,2),(85,20,1),(86,20,2),(87,20,3),(88,5,1),(89,5,2),(90,5,3),(91,5,7),(92,5,8),(93,5,9),(94,5,10),(95,5,11),(96,5,12),(97,5,13),(98,5,14),(99,22,5),(100,22,6),(101,22,12),(102,22,13);
+/*!40000 ALTER TABLE `publisher_book` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-10-12  2:15:06
---
+++ index.php
@@ -0,0 +1,268 @@
+<?php
+require_once 'dictator.php';
+
+$pdo = new \PDO(
+  'mysql:host=localhost;dbname=dictator',
+  'root', '', array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+  )
+);
+
+$dictator = new \Dictator;
+$dictator
+  ->setConnection($pdo)
+  
+  ->setOptions(array(
+    'Actions' => 'Действия',
+    'Edit' => 'Редактировать',
+    'Delete' => 'Удалить',
+    'New row' => 'Новая запись',
+    'Edit row' => 'Редактировать запись',
+    'Page' => 'Страница',
+    'Sure?' => 'Уверены?',
+    'Search' => 'Найти',
+    'Save' => 'Сохранить',
+    'List rows' => 'Все записи',
+    'Saved successful' => 'Успешно сохранено',
+    'ipp' => 5
+  ))
+
+  ->setTable('book', 'Книги')
+
+  ->addField('id', 'ID')
+  ->addField('pic', 'Обложка')
+  ->addField('categoryId', 'Категория')
+  ->addField('name', 'Название')
+  ->addField('desc', 'Описание')
+  ->addField('date', 'Дата')
+
+  ->addFilter('pic', function( $text ) {
+    if ( $text ) {
+      return '<img width="150" src="upload/' . $text . '">';
+    }
+
+    return '—';
+  })
+  ->addFilter('desc', function( $text ) {
+    if ( $text ) {
+      $substr = mb_substr($text, 0, 200, 'UTF-8');
+      return $substr . '…';
+    }
+
+    return '—';
+  })
+  ->addFilter('date', function( $text ) {
+    if ( $text ) {
+      return date( 'd.m.Y (H:i:s)', strtotime($text) );
+    }
+
+    return '—';
+  })
+
+  ->addFileWidget('pic', __DIR__ . '/upload')
+
+  ->addTextWidget('desc')
+
+  ->addRelation('categoryId', 'category', 'name')
+  
+  ->addManyToManyRelation(
+    'author', 'name', 'Авторы',
+    'author_book', 'bookId', 'authorId'
+  )
+
+  ->addManyToManyRelation(
+    'publisher', 'name', 'Издатели',
+    'publisher_book', 'bookId', 'publisherId'
+  )
+
+  ->addSearch('name')
+  ->addSearch('desc')
+  ;
+
+  $html = $dictator->generate();
+?>
+
+
+<html>
+<head>
+  <title>Dictator Demo</title>
+
+  <!--[if lt IE 9]>
+  <style>
+    table.dictator th {
+      filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#d5e3e4', endColorstr='#b3c8cc',GradientType=0 );
+      position: relative;
+      z-index: -1;
+    }
+    table.dictator td {
+      filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ebecda', endColorstr='#ceceb7',GradientType=0 );
+      position: relative;
+      z-index: -1;
+    }
+  </style>
+  <![endif]-->
+</head>
+<body>
+  <style>
+    .dictator-sort-asc, .dictator-sort-desc {
+      padding-left: 10px;
+      background-repeat: no-repeat;
+      background-position: -6px -3px;
+    }
+    .dictator-sort-desc {
+      background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAABZ0RVh0Q3JlYXRpb24gVGltZQAwNC8xNC8wOFZL9aYAAAAcdEVYdFNvZnR3YXJlAEFkb2JlIEZpcmV3b3JrcyBDUzQGstOgAAAAj0lEQVQ4jWP8//8/AzUBE1VNGzVwhBjIgk9yr4AAzkTq/OEDI8kGMjAwMJhWVGCIne7owKker5ddPn5khGn+sX8/3LBc459YXcfAwMDA8P//f5wYJr+Hn///x/b2/3v4+f/jU////38GRnx5mZER4ZA9/Pz/t0+RZfr+8zMTAwPD36nJD7DrGS0cRg0kHQAAYMtZjBTUt+kAAAAASUVORK5CYII=');
+    }
+    .dictator-sort-asc {
+      background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAABZ0RVh0Q3JlYXRpb24gVGltZQAwNC8xNC8wOFZL9aYAAAAcdEVYdFNvZnR3YXJlAEFkb2JlIEZpcmV3b3JrcyBDUzQGstOgAAAApUlEQVQ4je2TMQ7CMAxFv6MKIYbEV2HIASquwcYhmhv0BD0NibpxDkbGsrGkZkB0ajxAByT6JUuWv/6TLNkkIlhSZlHaCvwTYKWZRDT10Tm57XfV5bgBgNydrvMhESnW24/Oyb1tJTJLtM5oGXXls7WUmMWHgEffwzcNYCgn5mJOBRLR6EMAAGzrGgBeUCAXM9ovJ+aieRgGmpurwE/0+3e4Ar/XEybDVp50x3YfAAAAAElFTkSuQmCC');
+    }
+    table.dictator {
+      font-family: verdana,arial,sans-serif;
+      font-size:11px;
+      color:#333333;
+      border-width: 1px;
+      border-color: #999999;
+      border-collapse: collapse;
+      box-shadow: 0 0 10px #aaa;
+      width: 100%;
+    }
+    table.dictator th a {
+      color: #000;
+    }
+    table.dictator th {
+      padding: 0px;
+      background: #d5e3e4;
+      background: url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/Pgo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgdmlld0JveD0iMCAwIDEgMSIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+CiAgPGxpbmVhckdyYWRpZW50IGlkPSJncmFkLXVjZ2ctZ2VuZXJhdGVkIiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSIgeDE9IjAlIiB5MT0iMCUiIHgyPSIwJSIgeTI9IjEwMCUiPgogICAgPHN0b3Agb2Zmc2V0PSIwJSIgc3RvcC1jb2xvcj0iI2Q1ZTNlNCIgc3RvcC1vcGFjaXR5PSIxIi8+CiAgICA8c3RvcCBvZmZzZXQ9IjQwJSIgc3RvcC1jb2xvcj0iI2NjZGVlMCIgc3RvcC1vcGFjaXR5PSIxIi8+CiAgICA8c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiNiM2M4Y2MiIHN0b3Atb3BhY2l0eT0iMSIvPgogIDwvbGluZWFyR3JhZGllbnQ+CiAgPHJlY3QgeD0iMCIgeT0iMCIgd2lkdGg9IjEiIGhlaWdodD0iMSIgZmlsbD0idXJsKCNncmFkLXVjZ2ctZ2VuZXJhdGVkKSIgLz4KPC9zdmc+);
+      background: -moz-linear-gradient(top,  #d5e3e4 0%, #ccdee0 40%, #b3c8cc 100%);
+      background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#d5e3e4), color-stop(40%,#ccdee0), color-stop(100%,#b3c8cc));
+      background: -webkit-linear-gradient(top,  #d5e3e4 0%,#ccdee0 40%,#b3c8cc 100%);
+      background: -o-linear-gradient(top,  #d5e3e4 0%,#ccdee0 40%,#b3c8cc 100%);
+      background: -ms-linear-gradient(top,  #d5e3e4 0%,#ccdee0 40%,#b3c8cc 100%);
+      background: linear-gradient(to bottom,  #d5e3e4 0%,#ccdee0 40%,#b3c8cc 100%);
+      border: 1px solid #999999;
+    }
+    table.dictator td {
+      padding: 0px;
+      background: #ebecda;
+      background: url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/Pgo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgdmlld0JveD0iMCAwIDEgMSIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+CiAgPGxpbmVhckdyYWRpZW50IGlkPSJncmFkLXVjZ2ctZ2VuZXJhdGVkIiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSIgeDE9IjAlIiB5MT0iMCUiIHgyPSIwJSIgeTI9IjEwMCUiPgogICAgPHN0b3Agb2Zmc2V0PSIwJSIgc3RvcC1jb2xvcj0iI2ViZWNkYSIgc3RvcC1vcGFjaXR5PSIxIi8+CiAgICA8c3RvcCBvZmZzZXQ9IjQwJSIgc3RvcC1jb2xvcj0iI2UwZTBjNiIgc3RvcC1vcGFjaXR5PSIxIi8+CiAgICA8c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiNjZWNlYjciIHN0b3Atb3BhY2l0eT0iMSIvPgogIDwvbGluZWFyR3JhZGllbnQ+CiAgPHJlY3QgeD0iMCIgeT0iMCIgd2lkdGg9IjEiIGhlaWdodD0iMSIgZmlsbD0idXJsKCNncmFkLXVjZ2ctZ2VuZXJhdGVkKSIgLz4KPC9zdmc+);
+      background: -moz-linear-gradient(top,  #ebecda 0%, #e0e0c6 40%, #ceceb7 100%);
+      background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#ebecda), color-stop(40%,#e0e0c6), color-stop(100%,#ceceb7));
+      background: -webkit-linear-gradient(top,  #ebecda 0%,#e0e0c6 40%,#ceceb7 100%);
+      background: -o-linear-gradient(top,  #ebecda 0%,#e0e0c6 40%,#ceceb7 100%);
+      background: -ms-linear-gradient(top,  #ebecda 0%,#e0e0c6 40%,#ceceb7 100%);
+      background: linear-gradient(to bottom,  #ebecda 0%,#e0e0c6 40%,#ceceb7 100%);
+      border: 1px solid #999999;
+    }
+    table.dictator th p{
+      margin:0px;
+      padding:8px;
+      border-bottom:0px;
+      border-right:0px;
+    }
+    table.dictator td p {
+      margin:0px;
+      padding:8px;
+      border-bottom:0px;
+      border-right:0px;
+    }
+    .dictator-pager {
+      margin-top: 10px;
+    }
+    .dictator-current {
+      background-color: #fff000;
+    }
+    .dictator-page, .dictator-current {
+      padding: 3px;
+    }
+    .dictator-pager a {
+      color: #000;
+    }
+
+    .dictator-search {
+      background-color: #fafafa;
+      padding: 10px;
+    }
+
+    .dictator-search input[type=text] {
+      border: 1px solid #aaa;
+      padding: 5px;
+    }
+    
+    .dictator-search input[type=submit] {
+      padding: 5px 15px;
+      border: 1px solid #aaa;
+      background-color: #fff;
+    }
+
+    .dictator-search input[type=submit]:hover {
+      opacity: 0.8;
+      cursor: pointer;
+      background-color: #333;
+      color: #fff;
+    }
+
+    .dictator-search label {
+      margin-right: 20px;
+    }
+
+    .dictator-row-m2m {
+      border: 1px solid #ddd;
+      display: inline-block;
+      max-height: 100px;
+      overflow-y: scroll;
+      padding: 5px;
+    }
+
+    .dictator-row {
+      margin-bottom: 10px;
+    }
+
+    .dictator-row input[type=text] {
+      border: 1px solid #aaa;
+      padding: 5px;
+    }
+
+    input[type=submit],
+    .dictator-row select {
+      padding: 5px;
+    }
+
+    .dictator-list-rows {
+      margin-left: 10px;
+    }
+
+    .dictator-row textarea {
+      border: 1px solid #aaa;
+      padding: 5px;
+      height: 200px;
+      width: 500px;
+    }
+
+    .dictator-saved {
+      background-color: rgb(208, 238, 118);
+      padding: 10px;
+      margin-bottom: 20px;
+    }
+  </style>
+
+  <?php echo $html ?>
+
+  <script src="jquery-1.10.2.min.js"></script>
+  <script>
+  $(document).on('click', 'th input[type=checkbox]', function() {
+    if ( $(this).is(':checked') ) {
+      $('td input[type=checkbox]').prop('checked', true);
+    } else {
+      $('td input[type=checkbox]').prop('checked', false);
+    }
+  });
+
+  $(document).on('click', 'td input[type=checkbox]', function() {
+    if( $('td input[type=checkbox]:checked').length == $('td input[type=checkbox]').length ) {
+      $('th input[type=checkbox]').prop('checked', true);
+    } else {
+      $('th input[type=checkbox]').prop('checked', false);
+    }
+  });
+  </script>
+</body>
+</html>
